1295add7394f01c1d77cf0ee8e0fed4c29624281
Carlos Fabián Melgarejo Agudelo <cmelgarejobm@gmail.com>
2023-10-22 09:08:14 -0500

    Menu Mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.svg";
+import menuImage from "../assets/menu.svg";
 import { animateScroll } from "react-scroll";
 import "./style.css";
 
 export default function NavBar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+
   const scrollToInfo = () => {
     const section = document.querySelector(".info");
     animateScroll.scrollTo(section.offsetTop);
@@ -29,7 +36,27 @@ export default function NavBar() {
             <button id="loginButton">Iniciar Sesión</button>
             <button id="signupButton">Registrarse</button>
           </div>
+          <div className="navBarMobile">
+            <img
+              id="imageNavBarMobile"
+              src={menuImage}
+              alt="menu.svg"
+              onClick={handleClick}
+            />
+          </div>
         </div>
+        {showMenu ? (
+          <div id="menuMobile">
+            <button id="noBackgrounButton" onClick={scrollToInfo}>
+              ¿Cómo funciona?
+            </button>
+            <button id="noBackgrounButton" onClick={scrollToContact}>
+              Contacto
+            </button>
+            <button id="loginButton">Iniciar Sesión</button>
+            <button id="signupButton">Registrarse</button>
+          </div>
+        ) : null}
       </div>
     </>
   );
